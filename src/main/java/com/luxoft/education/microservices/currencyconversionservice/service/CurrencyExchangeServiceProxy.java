7943d2e00a9ec9c2762d938b5eaf15bd7d0ee610
VasiliKnyazev <basil_jr_king@mail.ru>
2021-01-11 13:10:59 +0600

    Added support for Zuul Api Gateway

--- src/main/java/com/luxoft/education/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/luxoft/education/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -9,11 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //use without ribbon
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//use with ribbon, but without zuul
+//@FeignClient(name = "currency-exchange-service")
+//use with ribbon and with zuul
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //use with or without ribbon and without zuul
+    //@GetMapping("/currency-exchange/from/{from}/to/{to}")
+    //use with ribbon and with zuul
+    @GetMapping("currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionResult retrieveExchangeValue(@PathVariable("from") String from,
                                                    @PathVariable("to") String to);
 
